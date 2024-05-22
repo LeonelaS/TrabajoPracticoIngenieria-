457918aa2a50c9ed7bb1534f9cfa056f83ce46a4
Juani <juani.flami@gmail.com>
2024-05-22 17:44:34 -0300

    normalizador añadido tambien a barra de busqueda.

---
+++ buscadorDireccion.js
@@ -0,0 +1,48 @@
+// este funciona, y procesa todas las opciones para pasarselas al html en un arreglo.
+// pero al elegir alguna opcion no pasa nada.
+function buscarDireccion() {
+    // Obtener la dirección ingresada por el usuario
+    var direccion = document.getElementById('search-input').value;
+    console.log("direcicon base obtenida");
+
+    // URL de la API de normalización de direcciones de USIG
+    var apiUrl = 'http://servicios.usig.buenosaires.gob.ar/normalizar?direccion=' + encodeURIComponent(direccion);
+    console.log("URL api usig obtenida::::" + apiUrl);
+
+    // Realizar la solicitud HTTP a la API
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log("entrando a realizar la solicitud http a la api");
+            // Procesar la respuesta de la API para obtener las direcciones normalizadas
+            var direcciones = data.direccionesNormalizadas; // Ajusta esto según la estructura de la respuesta de la API
+
+            if (direcciones && direcciones.length > 0) {
+                var select = document.getElementById('direccionBuscadaNormalizada');
+                select.innerHTML = ''; // Limpiar opciones anteriores
+
+                console.log("devolviendo respuesta de la api para elegir de las opciones");
+                // Añadir una opción predeterminada
+                var option = document.createElement('option');
+                option.value = '';
+                option.text = 'Seleccione una dirección';
+                select.appendChild(option);
+
+                console.log("añadiendo respuesta de la api al campo de seleccion");
+                // Añadir todas las direcciones normalizadas al campo de selección
+                direcciones.forEach(direccionObj => {
+                    var option = document.createElement('option');
+                    option.value = direccionObj.direccion; // Ajusta esto según el formato de las direcciones en la respuesta
+                    option.text = direccionObj.direccion; // Ajusta esto según el formato de las direcciones en la respuesta
+                    select.appendChild(option);
+                });
+            } else {
+                alert('No se encontraron direcciones normalizadas.');
+            }
+        })
+        .catch(error => {
+            // Manejar errores de la solicitud
+            console.error('Error al normalizar la dirección:', error);
+            alert('Hubo un error al normalizar la dirección. Por favor, inténtalo de nuevo más tarde.');
+        });
+}
--- mapa.js
+++ mapa.js
@@ -1,61 +1,103 @@
-// Inicializar el mapa
-var map = L.map('map').setView([-34.523013308639, -58.70045616768699], 15); // Coordenadas de la Universidad Nacional de General Sarmiento
+// // Inicializar el mapa
+// var map = L.map('map').setView([-34.523013308639, -58.70045616768699], 15); // Coordenadas de la Universidad Nacional de General Sarmiento
 
-// Añadir una capa de mapa base
+// // Añadir una capa de mapa base
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+//     maxZoom: 19,
+// }).addTo(map);
+
+// // Añadir un marcador
+// L.marker([-34.523013308639, -58.70045616768699]).addTo(map)
+//     .bindPopup('Universidad Nacional de General Sarmiento')
+//     .openPopup();
+
+//     // Variable global para almacenar el marcador actual
+// var currentMarker;
+
+// // Función para geocodificar la dirección y mostrarla en el mapa
+// function mostrarDireccionEnMapa() {
+//     var direccion = document.getElementById('direccionNormalizada').value;
+
+//     if (!direccion) {
+//         alert('Seleccione una dirección');
+//         return;
+//     }
+
+//     // URL de la API de geocodificación de USIG
+//     var apiUrl = 'http://servicios.usig.buenosaires.gob.ar/normalizar?direccion=' + encodeURIComponent(direccion);
+
+//     fetch(apiUrl)
+//         .then(response => response.json())
+//         .then(data => {
+//             if (data.direccionesNormalizadas && data.direccionesNormalizadas.length > 0) {
+//                 var direccionNormalizada = data.direccionesNormalizadas[0];
+
+//                 // Obtener las coordenadas de la dirección normalizada
+//                 var coordenadas = L.latLng(direccionNormalizada.y, direccionNormalizada.x);
+
+//                 // Mover el mapa a las coordenadas de la dirección normalizada
+//                 map.setView(coordenadas, 15);
+
+//                 // Si ya existe un marcador, eliminarlo
+//                 if (currentMarker) {
+//                     map.removeLayer(currentMarker);
+//                 }
+
+//                 // Añadir un marcador en la dirección normalizada
+//                 currentMarker = L.marker(coordenadas).addTo(map)
+//                     .bindPopup("espacio fisico del colaborador!!<br>" + direccionNormalizada.direccion)
+//                     .openPopup();
+//             } else {
+//                 alert('No se encontraron coordenadas para la dirección seleccionada.');
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error al geocodificar la dirección:', error);
+//             alert('Hubo un error al geocodificar la dirección. Por favor, inténtalo de nuevo más tarde.');
+//         });
+// }
+
+
+
+var map = L.map('map').setView([-34.523013308639, -58.70045616768699], 15);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
     maxZoom: 19,
 }).addTo(map);
 
-// Variable global para almacenar el marcador actual
+// Añadir un marcador
+L.marker([-34.523013308639, -58.70045616768699]).addTo(map)
+    .bindPopup('Universidad Nacional de General Sarmiento')
+    .openPopup();
+
 var currentMarker;
 
-// Función para geocodificar la dirección y mostrarla en el mapa
+// este no vuelve a hacerle la misma petición a la api
 function mostrarDireccionEnMapa() {
-    var direccion = document.getElementById('direccionNormalizada').value;
+    var select = document.getElementById('direccionNormalizada');
+    var direccion = JSON.parse(select.value); // Parsear el objeto completo
 
     if (!direccion) {
         alert('Seleccione una dirección');
         return;
     }
 
-    // URL de la API de geocodificación de USIG
-    var apiUrl = 'http://servicios.usig.buenosaires.gob.ar/normalizar?direccion=' + encodeURIComponent(direccion);
+    var coordenadas = [direccion.lat, direccion.lon];
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            if (data.direccionesNormalizadas && data.direccionesNormalizadas.length > 0) {
-                var direccionNormalizada = data.direccionesNormalizadas[0];
+    if (coordenadas[0] && coordenadas[1]) {
+        map.setView(coordenadas, 15);
 
-                // Obtener las coordenadas de la dirección normalizada
-                var coordenadas = L.latLng(direccionNormalizada.y, direccionNormalizada.x);
+        if (currentMarker) {
+            map.removeLayer(currentMarker);
+        }
 
-                // Mover el mapa a las coordenadas de la dirección normalizada
-                map.setView(coordenadas, 15);
-
-                // Si ya existe un marcador, eliminarlo
-                if (currentMarker) {
-                    map.removeLayer(currentMarker);
-                }
-
-                // Añadir un marcador en la dirección normalizada
-                currentMarker = L.marker(coordenadas).addTo(map)
-                    .bindPopup("espacio fisico del colaborador!!<br>" + direccionNormalizada.direccion)
-                    .openPopup();
-            } else {
-                alert('No se encontraron coordenadas para la dirección seleccionada.');
-            }
-        })
-        .catch(error => {
-            console.error('Error al geocodificar la dirección:', error);
-            alert('Hubo un error al geocodificar la dirección. Por favor, inténtalo de nuevo más tarde.');
-        });
+        currentMarker = L.marker(coordenadas).addTo(map)
+            .bindPopup(direccion.direccion)
+            .openPopup();
+    } else {
+        alert('No se encontraron coordenadas para la dirección seleccionada.');
+    }
 }
-
-
-// Añadir un marcador
-L.marker([-34.523013308639, -58.70045616768699]).addTo(map)
-    .bindPopup('Universidad Nacional de General Sarmiento')
-    .openPopup();
 
